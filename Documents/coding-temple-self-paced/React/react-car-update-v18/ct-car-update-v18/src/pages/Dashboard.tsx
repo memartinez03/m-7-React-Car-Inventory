9d652ec3232ca0c1ff0201b8c4454423cf572e3d
memartinez03 <memartinez0361@gmail.com>
2023-05-18 23:51:21 -0700

    uplaoding files

---
+++ Documents/coding-temple-self-paced/React/react-car-update-v18/ct-car-update-v18/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import DataTable from "../components/DataTable"
+
+function Dashboard() {
+  return (
+    <div><DataTable/></div>
+  )
+}
+
+export default Dashboard
---
+++ Documents/coding-temple-self-paced/React/react-car-update-v18/ct-car-update-v18/src/redux/store.ts
@@ -0,0 +1,7 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { reducer } from "./slices/RootSlice"
+
+export const store = configureStore({
+    reducer,
+    devTools: true,
+})
