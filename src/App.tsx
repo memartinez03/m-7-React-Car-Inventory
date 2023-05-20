fd79dca1a453fc1d67b48d89bf01979540131ef7
memartinez03 <106893983+memartinez03@users.noreply.github.com>
2023-05-20 14:31:33 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, HashRouter, Routes, Route, Link } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import routes from './config/routes'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+
+import AuthChecker from './auth/AuthChecker'
+
+function App() {
+
+  return (
+    <HashRouter>
+      <Navbar />
+        <Provider store={store}>
+          <Routes>
+            { routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  route.protected ? (
+                  <AuthChecker>
+                    <route.component />
+                  </AuthChecker>
+                  ) : (
+                    <route.component />
+                  )
+                }
+                />
+            )) }
+          </Routes>
+        </Provider>
+    </HashRouter>
+  )
+}
+
+export default App
